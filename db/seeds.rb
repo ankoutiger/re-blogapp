744c97f338f30910c0be5e463df5e0f83d1a4f0f
こうへい <rugby.kohei.warabies@gmail.com>
2023-06-21 22:41:09 +0900

    トップページのUIを変更しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Article.create(title: 'aaaaaaa', content: 'こんにちは！')
+Article.create(title: '３つ目の記事だよ', content: '聞いてる？')
