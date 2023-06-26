21622f8267a1f938b2383bfb0b400e0813e5f3cd
こうへい <rugby.kohei.warabies@gmail.com>
2023-06-27 07:55:22 +0900

    アイキャッチの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,6 +14,8 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
+  has_one_attached :eyecatch
+
   validates :title, presence: true
   validates :content, presence: true
 
